fb33ba4c3a07225a570dbf2e11e1b053f4e9fb17
Felix Beekman <fbeekman@upcmail.nl>
2021-08-17 11:58:17 +0200

    added form

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 def projects(request):
-	page = 'projects'
-	number = 10
-	context = {'page':page, 'number':number}
+	projects = Project.objects.all()
+	context = { 'projects': projects}
 	return render(request, 'projects/projects.html', context)
 
 def project(request,pk):
-	return render(request, 'projects/single-project.html')
+	projectObj = Project.objects.get(id=pk)
+	print('projectObj:', projectObj)
+	return render(request, 'projects/single-project.html', {'project': projectObj})
 
+
+def createProject(request):
+	form = ProjectForm()
+	context = {'form':form}
+	return render(request, 'projects/project_form.html', context)
